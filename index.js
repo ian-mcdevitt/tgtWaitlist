cf3d3be7b600263148a897478e64510a1af8a94d
Ian McDevitt <ian@mcdevi.tt>
2021-03-30 17:53:26 -0400

    Add waitlisting and blacklisting

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var Discord = require('discord.js');
 var logger = require('winston');
 var auth = require('./auth.json');
+var config = require('./config.json');
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
@@ -17,71 +18,117 @@ client.once('ready', () => {
 
 client.login(auth.token);
 
-const signupSplit = '\n=== Sign-up List ===';
-const waitlistSplit = '\n=== Wait List ===';
-
 client.on('message', async triggerMessage => {
-    if (triggerMessage.content.startsWith('!job ')) {
-        var post = `<@&814324717485293611>, there's a new job for you!\n${triggerMessage.content.slice(5)}`;
+    if (triggerMessage.content.startsWith('!job')) {
+        // Check for a blacklist (to copy into our new post)
+        const blacklistIndex = triggerMessage.content.indexOf('--blacklist ');
+        const blacklist = blacklistIndex !== -1 ? `${config.blacklistHeader}${triggerMessage.content.substring(blacklistIndex + 12)}\n` : '';
+
+        var post = `${blacklist}<@&814324717485293611>, there's a new job for you!`;
 
         triggerMessage.channel.send(post).then(async jobMessage => {
-            await jobMessage.react('✅');
+            await jobMessage.react(config.signupEmoji);
+            await jobMessage.react(config.waitlistEmoji);
+            await jobMessage.react(config.notInterestedEmoji);
             await triggerMessage.delete();
         });
     }
 });
 
-const createUserList = (users) => {
-    let count = 0;
-    return users.map((user, index) => {
-        // This is the bot's own ID
-        if (user.id === '826446490346979349') {
-            return ''
-        } else {
-            let waitlist = '';
-            if (count === 5) {
-                waitlist = waitlistSplit;
-            }
-            return `${waitlist}\n${++count}.) ${user.toString()}`;
+const fetchReaction = async (reaction) => {
+    if (reaction.partial) {
+        try {
+            await reaction.fetch();
+        } catch (error) {
+            console.error('Something went wrong when fetching the message:', error);
+            return;
         }
+    }
+}
+
+const renderLists = (reactions) => {
+    let signupReactions = [];
+    let waitlistReactions = [];
+    // .map the collections to turn them into arrays (easier to work with)
+    // .slice(1) both arrays to remove the bot's reactions
+    reactions.map((reaction, index) => {
+        if (index === config.signupEmoji) {
+            signupReactions = reaction.users.cache.map(user => user).slice(1);
+        } else if (index === config.waitlistEmoji) {
+            waitlistReactions = reaction.users.cache.map(user => user).slice(1);
+        }
+    })
+
+    // Move anyone over the 5th player into the waitlist (before people who deliberately waitlisted themselves)
+    if (signupReactions.length > 5) {
+        waitlistReactions = [...signupReactions.slice(5), ...waitlistReactions];
+        signupReactions = signupReactions.slice(0, 5);
+    }
+
+    // Create the list string
+    let lists = '';
+    if (signupReactions.length > 0) {
+        lists += config.signupSplit + listUsers(signupReactions);
+    }
+    if (waitlistReactions.length > 0) {
+        lists += config.waitlistSplit + listUsers(waitlistReactions);
+    }
+    return lists;
+}
+
+const listUsers = (users) => {
+    return users.map((user, index) => {
+        return `\n${index + 1}.) ${user.toString()}`;
     }).join('');
 }
 
 client.on('messageReactionAdd', async reaction => {
-    if (reaction.partial) {
-        try {
-            await reaction.fetch();
-        } catch (error) {
-            console.error('Something went wrong when fetching the message:', error);
-            return;
-        }
+    let botMessage = reaction.message.content;
+
+    await fetchReaction(reaction);
+
+    if (![config.signupEmoji, config.waitlistEmoji, config.notInterestedEmoji].includes(reaction.emoji.id || reaction.emoji.name)) {
+        return reaction.remove();
     }
 
-    if (reaction.emoji.name === '✅') {
-        console.info('Adding a user to the list!');
-        // Add the user to the list
-        const originalText = reaction.message.content.split(signupSplit)[0];
-        const userList = createUserList(reaction.users.cache);
-        await reaction.message.edit(originalText + (userList.length ? signupSplit : '') + userList);
-    } else {
-        await reaction.remove();
+    // Delete reactions from this channel that are on any post other than this bot's
+    if (reaction.message.author.id !== config.botUserId) {
+        return reaction.remove();
     }
+
+    // Fetch the blacklist from our own message
+    let blacklist = [];
+    if (botMessage.indexOf(config.blacklistHeader) === 0) {
+        blacklist = botMessage.substring(config.blacklistHeader.length, botMessage.indexOf('\n')).split(' ').map((user) => {
+            return user.trim().slice(3, -1);
+        })
+    }
+
+    // The blacklist period has not yet expired
+    if (+(new Date()) - reaction.message.createdTimestamp < config.blacklistDuration) {
+        reaction.users.cache.map(async (user, id) => {
+            if (blacklist.includes(user.id)) {
+                await reaction.users.remove(user.id);
+            }
+        })
+    // The blacklist has expired! Remove it from the message if it's still there
+    } else if (botMessage.indexOf(config.blacklistHeader) !== -1) {
+        botMessage = botMessage.substring(botMessage.indexOf('\n'));
+    }
+
+    // Cut out the lists
+    botMessage = botMessage.split(config.signupSplit)[0].split(config.waitlistSplit)[0];
+
+    await reaction.message.edit(botMessage + renderLists(reaction.message.reactions.cache));
 })
 
 client.on('messageReactionRemove', async reaction => {
-    if (reaction.partial) {
-        try {
-            await reaction.fetch();
-        } catch (error) {
-            console.error('Something went wrong when fetching the message:', error);
-            return;
-        }
-    }
+    let botMessage = reaction.message.content;
 
-    if (reaction.emoji.name === '✅') {
-        console.info('Removing a user from the list!');
-        const originalText = reaction.message.content.split(signupSplit)[0];
-        const userList = createUserList(reaction.users.cache);
-        await reaction.message.edit(originalText + (userList.length ? signupSplit : '') + userList);
-    }
+    await fetchReaction(reaction);
+
+    // Cut out the lists
+    botMessage = botMessage.split(config.signupSplit)[0].split(config.waitlistSplit)[0];
+
+    await reaction.message.edit(botMessage + renderLists(reaction.message.reactions.cache));
 });
